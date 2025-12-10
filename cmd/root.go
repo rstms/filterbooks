8eb3009b91b928ddf8efd634636b75ef46ceb7f1
Matt Krueger <mkrueger@rstms.net>
2025-12-10 04:54:20 -0700

    Bump version: 0.0.2 → 0.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -41,7 +41,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.1.0",
 	Use:     "filterbooks",
 	Short:   "dovecot sieve filter implmenting filter-books header manipulation",
 	Long: `
